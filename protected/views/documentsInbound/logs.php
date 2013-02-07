aa75234ca1f18f71168d1bd54880c8645bf3bb12
Alexander Schmidt <childofstars@gmail.com>
2013-02-07 11:19:19 +0100

    new view

---
+++ protected/views/documentsInbound/logs.php
@@ -0,0 +1,33 @@
+<?php
+
+/* @var $this DocumentsInboundController */
+/* @var $model DocumentsInbound */
+
+// TODO: create method getLogId that is the log id currently used
+
+$this->breadcrumbs=array(
+	'Documents Inbounds'=>array('index'),
+	$this->getLogId()->id,
+);
+
+?>
+
+<h1>View Document#<?php echo $model->id; ?> with log id#<?php echo $this->getLogId()->id; ?></h1>
+
+
+<?php 
+// creates a widget with the log information in the controller
+
+    $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$this->getLog(),
+	'attributes'=>array(
+		'id',
+		'log_code',
+		'document_table',
+		'document_id',
+		'date',
+		'message',
+		'cleaned_message',
+		'status',
+	),
+)); ?>
